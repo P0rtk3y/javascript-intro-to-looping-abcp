aacc0adb78a075d3a23a2348f3ed3968ac119689
Lana Worobec <llama.p0rtk3y@gmail.com>
2018-01-19 05:59:34 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop (array){
- for (var i =0; i < 25; i++){
+ for (var i =0; i < array; i++){
   if (i === 1){
     console.log("I am 1 strange loop");
   } else {
@@ -7,3 +7,5 @@ function forLoop (array){
   }
  }
 }
+
+forLoop()
